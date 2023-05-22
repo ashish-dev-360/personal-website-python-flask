c26c155a61fb71bf42ad823ac458e937553b556e
Ashish Kumar <123142050+ashish-dev-360@users.noreply.github.com>
2023-05-22 18:29:49 +0530

    Commit

---
+++ data.py
@@ -0,0 +1,90 @@
+DATA = {
+        'meta_title': "Ashish Kumar | Its all about me",
+        'description': "I am an experienced Web Developer with a demonstrated "
+                            "history of working in the information technology and services industry. "
+                            "Skilled in Javascript, Python, E-commerce, PHP, WordPress, Codeigniter "
+                            "and Customer Relationship Management (CRM). Strong engineering professional "
+                            "with a BCA focused in Software Engineering from Sikkim Manipal university",
+        'name': "Ashish Kumar",
+        'profession': "Senior Software Engineer",
+        'services': [
+            {
+                'title': 'Design',
+                'desc': 'Labore velit culpa adipisci excepturi consequuntur itaque in nam '
+                        'molestias dolorem iste quod.'
+            },
+            {
+                'title': 'Development',
+                'desc': 'Labore velit culpa adipisci excepturi consequuntur itaque in nam '
+                        'molestias dolorem iste quod.'
+            },
+            {
+                'title': 'Maintenance',
+                'desc': 'Labore velit culpa adipisci excepturi consequuntur itaque in nam '
+                        'molestias dolorem iste quod.'
+            },
+            {
+                'title': 'SEO & Analytics',
+                'desc': 'Labore velit culpa adipisci excepturi consequuntur itaque in nam '
+                        'molestias dolorem iste quod.'
+            }
+        ],
+        'portfolio': [
+                    {'title': 'Wordpress', 'image': 'static/imgs/folio-1.jpg', 'cat': 'eCommerce Website', 'link': '#'},
+                    {'title': 'PHP', 'image': 'static/imgs/folio-2.jpg', 'cat': 'CRM Software', 'link': '#'},
+                    {'title': 'Python', 'image': 'static/imgs/folio-3.jpg', 'cat': 'Blog Website', 'link': '#'},
+                ],
+        'testimonial': [
+                            {
+                                'client': 'Emily Reb',
+                                'image': 'static/imgs/avatar3.jpg',
+                                'desc': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. '
+                                        'Eaque doloribus autem aperiam earum nostrum omnis blanditiis '
+                                        'corporis perspiciatis adipisci nihil.'
+                            },
+                            {
+                                'client': 'Peter James',
+                                'image': 'static/imgs/avatar2.jpg',
+                                'desc': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. '
+                                        'Eaque doloribus autem aperiam earum nostrum omnis blanditiis '
+                                        'corporis perspiciatis adipisci nihil.'
+                            },
+                        ],
+        'blogs': [
+                    {
+                        'title': 'Consectetur adipisicing elit',
+                        'desc': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. '
+                                'Impedit excepturi laborum enim, vitae ipsam atque eum, ad iusto '
+                                'consequuntur voluptas, esse doloribus. Perferendis porro quisquam '
+                                'vitae exercitationem aliquid, minus eos laborum repudiandae, cumque '
+                                'debitis iusto omnis praesentium? Laborum placeat sit adipisci illum '
+                                'tempore maxime, esse qui quae? Molestias excepturi corporis similique'
+                                ' doloribus. Esse vitae earum architecto nulla non dolores illum at '
+                                'perspiciatis quod, et deleniti cupiditate reiciendis harum facere, '
+                                'delectus eum commodi soluta distinctio sit repudiandae possimus sunt. '
+                                'Ipsum, rem.',
+                        'slug': 'consectetur-adipisicing-elit',
+                        'author': 'Admin',
+                        'date': '19th Feb 2022',
+                        'image': 'static/imgs/img-1.jpg'
+                    },
+                    {
+                        'title': 'Lorem ipsum dolor sit amet',
+                        'desc': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. '
+                                'Impedit excepturi laborum enim, vitae ipsam atque eum, ad iusto '
+                                'consequuntur voluptas, esse doloribus. Perferendis porro quisquam '
+                                'vitae exercitationem aliquid, minus eos laborum repudiandae, cumque '
+                                'debitis iusto omnis praesentium? Laborum placeat sit adipisci illum '
+                                'tempore maxime, esse qui quae? Molestias excepturi corporis similique'
+                                ' doloribus. Esse vitae earum architecto nulla non dolores illum at '
+                                'perspiciatis quod, et deleniti cupiditate reiciendis harum facere, '
+                                'delectus eum commodi soluta distinctio sit repudiandae possimus sunt. '
+                                'Ipsum, rem.',
+                        'slug': 'lorem-ipsum-dolor-sit-amet',
+                        'author': 'Ashish',
+                        'date': '07th Dec 2021',
+                        'image': 'static/imgs/img-2.jpg'
+                    },
+                ]
+
+    }
